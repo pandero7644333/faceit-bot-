29ec21049b8a745de90d41ebffd69a747ad66ab8
pandero7644333 <szymon.pasternak6789@gmail.com>
2026-04-01 20:08:34 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+const { token } = require("./config");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+});
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync("./src/commands");
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync("./src/events");
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.login(token);
